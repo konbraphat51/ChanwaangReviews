7263c6518b524913e93c072c54d8a686a70f20e1
konbraphat51 <konbraphat@gmail.com>
2024-05-12 21:06:28 +0900

    add default component

---
+++ src/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="app">
+		{{ t("example.explain") }}
+
+		<!-- view page shown here -->
+		<router-view />
+	</div>
+</template>
+
+<script>
+export default Vue.defineComponent({
+	name: "App",
+	components: {
+		// LanguageSelection: Vue.defineAsyncComponent(() =>
+		// 	loadModule("src/components/LanguageSelection.vue", options),
+		// ),
+	},
+})
+</script>
---
+++ src/components/Home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="Home"></div>
+</template>
+
+<script>
+export default Vue.defineComponent({
+	name: "Home",
+	setup() {
+		//set up i18n
+		const {t} = VueI18n.useI18n()
+		return {t}
+	},
+})
+</script>
+
+<i18n>
+    {
+        "en": {
+            "example": {
+                "explain": "This is how you use i18n"
+            }
+        },
+        "ja": {
+            "example": {
+                "explain": "こうやってi18nを使います"
+            }
+        }
+    }
+</i18n>
