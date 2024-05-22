658477e231939c6775e791a05f759b5aabe8f21c
konbraphat51 <konbraphat@gmail.com>
2024-05-22 17:46:20 +0900

    read metadata

--- .github/workflows/ArticlesController.py
+++ .github/workflows/ArticlesController.py
@@ -1,4 +1,26 @@
 import glob
+import json
 
 # get all articles
 files = glob.glob("src/data/articles/each/*.md")
+
+#get all metadata
+metadata = []
+for file in files:
+    with open(file, 'r') as f:
+        content = f.read()
+        
+        # get metadata
+        # between "<!-- META" and "META -->"
+        START_SYMBOL = "<!-- META"
+        END_SYMBOL = "META -->"
+        start_index = content.find(START_SYMBOL)
+        end_index = content.find(END_SYMBOL)
+        metadatumString = content[start_index + len(START_SYMBOL) : end_index]
+        
+        # JSON to dictionary
+        metadatum = json.loads(metadatumString)
+        
+        # into list
+        metadata.append(metadatum)
+        
