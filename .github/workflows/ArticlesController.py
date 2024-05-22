e5c27c506f0db83dc442bdf9d40c4cf3a836ab89
konbraphat51 <konbraphat@gmail.com>
2024-05-22 17:58:44 +0900

    black

--- .github/workflows/ArticlesController.py
+++ .github/workflows/ArticlesController.py
@@ -5,12 +5,12 @@ import pathlib
 # get all articles
 files = glob.glob("src/data/articles/each/*.md")
 
-#get all metadata
+# get all metadata
 metadata = []
 for file in files:
-    with open(file, 'r', encoding="utf-8") as f:
+    with open(file, "r", encoding="utf-8") as f:
         content = f.read()
-        
+
         # get metadata
         # between "<!-- META" and "META -->"
         START_SYMBOL = "<!-- META"
@@ -18,21 +18,23 @@ for file in files:
         start_index = content.find(START_SYMBOL)
         end_index = content.find(END_SYMBOL)
         metadatumString = content[start_index + len(START_SYMBOL) : end_index]
-        
+
         # JSON to dictionary
         metadatum = json.loads(metadatumString)
-        
-        #get filename
+
+        # get filename
         path = pathlib.Path(file)
         filename = path.stem
         metadatum["filename"] = filename
-        
+
         # into list
         metadata.append(metadatum)
 
-#save to articles.json
-articles = {
-    "articles": metadata
-}
+# save to articles.json
+articles = {"articles": metadata}
 
-json.dump(articles, open("src/data/articles/articles.json", "w", encoding="utf-8"), indent=4)
+json.dump(
+    articles,
+    open("src/data/articles/articles.json", "w", encoding="utf-8"),
+    indent=4,
+)
