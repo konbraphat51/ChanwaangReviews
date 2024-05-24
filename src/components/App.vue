29abacb461a01d5415554045de9df0a8e59ec6d2
konbraphat51 <konbraphat@gmail.com>
2024-05-25 00:06:18 +0900

    add waanger link

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,6 +2,13 @@
 	<div id="app">
 		<!-- view page shown here -->
 		<router-view />
+
+		<a
+			href="https://konbraphat51.github.io/Waanger/"
+			target="_blank"
+			class="WaangerLink"
+			>Konbraphat51 วางer</a
+		>
 	</div>
 </template>
 
