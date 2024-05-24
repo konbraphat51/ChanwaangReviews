ef60cf1f7baf507dd6256e7f8e26e4c561865d94
konbraphat51 <konbraphat@gmail.com>
2024-05-24 21:59:31 +0900

    Filter component
    only logic

---
+++ src/components/ArticleTable/Filter.vue
@@ -0,0 +1,40 @@
+<template></template>
+
+<script>
+export default {
+	name: "Filter",
+	data() {
+		return {
+			filter: {
+				title: [],
+				media: [],
+				tags: [],
+				short: [],
+				hasPage: [],
+				minImportance: 0,
+				maxImportance: 5,
+				minCreatedTime: 0,
+				maxCreatedTime: 100000000000000,
+				minUpdatedTime: 0,
+				maxUpdatedTime: 100000000000000,
+			},
+		}
+	},
+	methods: {
+		Fractionize(input) {
+			let splited = this._SplitList([input], " ")
+			splited = this._SplitList(splited, ",")
+			splited = this._SplitList(splited, "/")
+
+			return splited
+		},
+		_SplitList(list, splitter) {
+			const result = []
+			for (const item of list) {
+				result.push(...item.split(splitter))
+			}
+			return result
+		},
+	},
+}
+</script>
