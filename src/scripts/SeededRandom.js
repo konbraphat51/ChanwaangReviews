281be2fefacdcdc1101add9709479457f2f46547
konbraphat51 <konbraphat@gmail.com>
2024-06-04 17:05:01 +0900

    change random algorithm

--- src/scripts/SeededRandom.js
+++ src/scripts/SeededRandom.js
@@ -1,18 +1,22 @@
-//https://dl.acm.org/doi/10.1145/159544.376068
+//https://sbfl.net/blog/2017/06/01/javascript-reproducible-random/
 class SeededRandom {
 	constructor(seed) {
-		this.SetSeed(seed)
+		this.x = 123456789
+		this.y = 362436069
+		this.z = 521288629
+		this.w = seed
 	}
 
-	SetSeed(seed) {
-		this.seed = seed
-	}
-
+	// XorShift
 	Get(maxExclusive) {
-		const raw = (48271 * this.seed) % 2147483647
+		let t
 
-		this.seed += 1
+		t = this.x ^ (this.x << 11)
+		this.x = this.y
+		this.y = this.z
+		this.z = this.w
+		this.w = this.w ^ (this.w >>> 19) ^ (t ^ (t >>> 8))
 
-		return raw % maxExclusive
+		return Math.abs(this.w) % maxExclusive
 	}
 }
