5d2c91b3d5cbc60289c8bdf265ce70ec08d4ee31
konbraphat51 <konbraphat@gmail.com>
2024-06-04 17:24:20 +0900

    delete root slash

--- src/components/ArticleTable/Importance.vue
+++ src/components/ArticleTable/Importance.vue
@@ -1,11 +1,11 @@
 <template>
 	<span class="importance">
 		<span class="filled" v-for="index in value" :key="index">
-			<img src="/src/imgs/star_filled.svg" />
+			<img src="src/imgs/star_filled.svg" />
 		</span>
 
 		<span class="empty" v-for="index in 5 - value" :key="index">
-			<img src="/src/imgs/star_empty.svg" />
+			<img src="src/imgs/star_empty.svg" />
 		</span>
 	</span>
 </template>
