d11173ee515cb46dc69cf1da6b937e0af8f5b051
konbraphat51 <konbraphat@gmail.com>
2024-06-04 15:15:19 +0900

    importance component

---
+++ src/components/ArticleTable/Importance.vue
@@ -0,0 +1,23 @@
+<template>
+	<span class="importance">
+		<span class="filled" v-for="index in value" :key="index">
+			<img src="/src/imgs/star_filled.svg" />
+		</span>
+
+		<span class="empty" v-for="index in 5 - value" :key="index">
+			<img src="/src/imgs/star_empty.svg" />
+		</span>
+	</span>
+</template>
+
+<script>
+export default {
+	name: "Importance",
+	props: {
+		value: {
+			type: Number,
+			required: true,
+		},
+	},
+}
+</script>
